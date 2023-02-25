4deddcb6824565780c41062224be4789ea3b2e42
Vaibhav Kubre <v@kubre.in>
2023-02-25 16:55:25 +0530

    Working Copy Finished

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.astro",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: "Roboto\\ Serif",
+        sans: "Roboto",
+      },
+    },
+  },
+  plugins: [],
+};
+
