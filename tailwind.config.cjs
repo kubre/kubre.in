1ce85e3207f52329e14df28d5d361a45b8ec4650
Vaibhav Kubre <v@kubre.in>
2023-02-26 09:31:24 +0530

    fonts and minor fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.astro",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        serif: "Roboto\\ Serif",
-        sans: "Roboto",
-      },
+    content: ["./src/**/*.astro"],
+    theme: {
+        extend: {
+            fontFamily: {
+                serif: "Roboto\\ Serif",
+                sans: "Roboto",
+                mono: ["Source\\ Code\\ Pro", "Consolas", "Menlo", "monospace"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
-
