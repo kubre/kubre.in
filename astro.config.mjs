075759378b10da5e7f686c20c02d9d884e281bb4
Vaibhav Kubre <v@kubre.in>
2023-05-07 14:55:37 +0530

    Added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    integrations: [tailwind()],
+    output: "server",
+    adapter: vercel({
+        analytics: true,
+    }),
 });
