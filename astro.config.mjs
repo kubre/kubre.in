c615e847fb49dcda9e01873909218ee5cbba4265
Vaibhav Kubre <v@kubre.in>
2023-05-14 12:20:38 +0530

    Blog posts now worl

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,17 @@ export default defineConfig({
     adapter: vercel({
         analytics: true,
     }),
+    markdown: {
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: "poimandres",
+            // Add custom languages
+            // Note: Shiki has countless langs built-in, including .astro!
+            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            langs: ['typescript', 'javascript'],
+            // Enable word wrap to prevent horizontal scrolling
+            wrap: true,
+        },
+    },
 });
