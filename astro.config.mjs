e03bf705bcf78dbb199332ba11225b1ef435ef7b
Vaibhav Kubre <vaibhav@192.168.1.3>
2026-01-26 14:15:41 +0530

    upgrade to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
     output: "static",
     adapter: cloudflare({
         imageService: 'cloudflare'
