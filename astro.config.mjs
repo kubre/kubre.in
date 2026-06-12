0a420809b79633139814ca8ced6a212cb0b65ece
Vaibhav Kubre <v@kubre.in>
2026-06-12 23:01:32 +0530

    Restore static Pages output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    output: "static",
-    adapter: cloudflare({
-        imageService: "passthrough",
-    }),
     site: "https://www.kubre.in",
     markdown: {
         shikiConfig: {
